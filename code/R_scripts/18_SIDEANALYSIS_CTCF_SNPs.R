89cd7c7b5d3fa357479e6722bf30c031c30e00d7
dvespasiani <dvespasiani@student.unimelb.edu.au>
2020-05-15 09:30:56 +1000

    changed script

--- code/R_scripts/18_SIDEANALYSIS_CTCF_SNPs.R
+++ code/R_scripts/18_SIDEANALYSIS_CTCF_SNPs.R
@@ -5,59 +5,49 @@ library(purrr)
 library(GenomicRanges)
 library(reshape)
 
-setwd('/data/projects/punim0586/dvespasiani/Files/PNG/')
+setwd('/data/projects/punim0586/dvespasiani/Files/PNG/Motifbreak/')
+
+output_dir='./CTCF_files/'
 
 tfbs=function(x){
-  as.character(list.files(x,full.names = T,recursive = F)) %>% 
+  x=as.character(list.files(x,full.names = T,recursive = F)) %>% 
     lapply(function(y)
       fread(y,sep=' ',header = T,
-            drop=c('Refpvalue','Altpvalue','dataSource'))[
-              ,start:=snpPos
-              ][
-                ,end:=start+1
-                ][
-                  ,c('snpPos'):=NULL
-                  ][geneSymbol%in%c('CTCFL','CTCF')]
-                      %>% unique() 
-    )
-}
-
-assign_pop_names=function(x){
-  pop_names=c('ambiguous_ctcf','denisova_ctcf','neandertal_ctcf','png_ctcf')
-  names(x)=pop_names
-  for (i in seq_along(x)){
-    assign(pop_names[i],x[[i]],.GlobalEnv)}
-  return(x)
-}
-
-ctcf_snps=tfbs('./Motifbreak/Temporary_tx_cres_combined/')
-ctcf_snps=assign_pop_names(ctcf_snps)
-
-# read original files
-read_snps=function(x){
-  as.character(list.files(x,full.names = T,recursive = F)) %>% 
-    lapply(function(y)
-      fread(y,sep=' ',header = T,select = c('seqnames','start','end','ANC','all_frequency','freq_range','CADD_PHRED','CADD_RAW')
+            drop=c('Refpvalue','Altpvalue'))[geneSymbol%like%'CTCF']
+                      %>% unique() %>% dplyr::select(c('seqnames','start','end',everything()))%>% as.data.table()
       )
-    )
+      x=x[c(1:3)] # dont consider naSNPs now
+    
+      pop_names=c('ambiguous','denisova','neandertal')
+      names(x)=pop_names
+      for (i in seq_along(x)){
+        assign(pop_names[i],x[[i]],.GlobalEnv)}
+      return(x)
 }
 
-ooa_snps=read_snps('./OoA_snps')
+
+ctcf_snps=tfbs('./Tx_and_CREs/TFBSs_disrupted_10neg5/combined/')
+
+
+## get rsids
+rsids=fread('../../human_genome/human_9606_b150_GRCh37p13_All_20170710_AllSNPs.gz',header = F,sep='\t',
+            col.names = c('seqnames','start','rsid','ref','alt'))
+rsids$seqnames=sub("^", "chr", rsids$seqnames)
+rsids=rsids[!seqnames%in%c('chrMT','chrX','chrY')][,c(1:3)]
+
 
 # combine
-keys=c('seqnames','start','end')
-lapply(ctcf_snps,function(x)setkeyv(x,keys))
-lapply(ooa_snps,function(x)setkeyv(x,keys))
+lapply(ctcf_snps,function(x)setkeyv(x,c('seqnames','start','end')))
+setkeyv(rsids,c('seqnames','start'))
 
-ctcf_snps=purrr::map2(ctcf_snps,ooa_snps,merge,by=keys)
-ctcf_snps=lapply(ctcf_snps,function(x)x=x[,c(1:3,5:7,20,21:24,9:19)] %>% unique())
+ctcf_snps=lapply(ctcf_snps,function(x)x=x[rsids,on=c('seqnames','start'),nomatch=0])
 
-write_list=function(x){
-lapply(names(x),function(y, x)
-  write.table(x[[y]],paste(y, "", sep = ""),row.names=FALSE, sep="\t", quote=F),x)
-}
+ctcf_snps=lapply(ctcf_snps,function(x)x=x[,delta_pwm:=abs(scoreRef-scoreAlt)] %>% group_by(`seqnames`,`start`,`end`) %>% 
+              filter(`delta_pwm`==max(delta_pwm)) %>% dplyr::select(-'delta_pwm')%>% as.data.table() %>% unique())
 
 
-setwd('/home/dvespasiani/')
-write_list(ctcf_snps)
+filenames=paste0(output_dir,names(ctcf_snps),sep='')
+mapply(write.table, ctcf_snps, file = filenames,col.names = T, row.names = F, sep = " ", quote = F)
+
+
 
